5eb0e6bb25894d5462669ea502c9c0bb3628fdeb
hindlet <79304662+hindlet@users.noreply.github.com>
2023-07-21 10:12:23 +0100

    updated example

--- examples/testing.rs
+++ examples/testing.rs
@@ -3,7 +3,7 @@ use scripting_lang::*;
 
 fn main() {
     
-    let items = create_item_list(vec!["Ore", "Bar"]);
+    let items = create_item_list(vec!["Ore", "Bar", "Stone", "Concrete"]);
 
     let mut buildings = create_buildings_list(vec![
         Building {
@@ -14,18 +14,31 @@ fn main() {
             pos: [5, 0],
             building_type: BuildingType::Factory((0, 2), (1, 1), (0, 0))
         },
+        Building {
+            pos: [10, 5],
+            building_type: BuildingType::TakeBox(2)
+        },
+        Building {
+            pos: [15, 0],
+            building_type: BuildingType::Factory((2, 2), (3, 1), (0, 0))
+        },
     ]);
 
     let builder = file_to_script("assets/script.gobbledygook");
 
-    let script = builder.bind_and_build(
+    let script_one = builder.bind_and_build(
         vec![("mine", 0), ("factory", 1)],
         vec![("raw", 0), ("processed", 1)]
+    );
+    let script_two = builder.bind_and_build(
+        vec![("mine", 2), ("factory", 3)],
+        vec![("raw", 2), ("processed", 3)]
     );
 
     let mut player = Player::new([0, 5]);
 
-    script.run_n(2, &mut player, &mut buildings, &items);
+    script_one.run_n(2, &mut player, &mut buildings, &items);
+    script_two.run_n(2, &mut player, &mut buildings, &items);
            
 
 
